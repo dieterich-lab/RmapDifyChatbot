3ffd0dd98755d7bb5ecf7af322122608fa6acf56
Philipp Wiesenbach <philipp.wiesenbach@uni-heidelberg.de>
2026-05-19 14:56:47 +0200

    Refactor: slim repository layout and rewrite README structure

--- dify_query/graphql_query.py
+++
@@ -1,8 +0,0 @@
-"""Compatibility wrapper for the old module name.
-
-Use dify_query.metadata_query for new imports.
-"""
-
-from dify_query.metadata_query import CODE_VERSION, main
-
-__all__ = ["main", "CODE_VERSION"]
